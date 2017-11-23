8eadfaaa8374a8b10b5e805377284f6c1c040edd
Al Jachimiak <aljachimiak@gmail.com>
2017-11-23 11:28:53 -0500

    adds test for a card

---
+++ test/card-test.js
@@ -0,0 +1,16 @@
+import test from 'ava';
+import Cards from '../cards';
+
+test('a card has a name, val, and suit', t => {
+	t.plan(3);
+	const deck = Cards.makeDeck(3);
+	const card = deck[0];
+
+	t.is('string', typeof (card.name()));
+
+	const val = (card.value).toString();
+	t.is(true, card.name().indexOf(val) > -1);
+
+	const suit = card.suit;
+	t.is(true, card.name().indexOf(suit) > -1);
+});
