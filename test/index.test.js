e1b41252c2926c2ec6c139d3d991d50a0b7f646d
Al Jachimiak <aljachimiak@gmail.com>
2018-02-02 18:34:31 -0500

    tests number of games returned bu play and promise many games

--- test/index.test.js
+++ test/index.test.js
@@ -18,6 +18,18 @@ test('playManyGames works', t => {
 	t.is(1000, gameData.length);
 });
 
+test('playManyGames plays specified amount of games', t => {
+	const gameData = Tgow.playManyGames(500);
+	t.plan(1);
+	t.is(500, gameData.length);
+});
+
+test('promiseManyGames plays specified amount of games', async t => {
+	const gameData = await Tgow.promiseManyGames(500);
+	t.plan(1);
+	t.is(500, gameData.length);
+});
+
 test('promiseManyGames works', async t => {
 	const manyGameData = await Tgow.promiseManyGames();
 	t.plan(1);
@@ -41,5 +53,3 @@ test.todo('in a game playhand calls compareTable');
 test.todo('in a game playhand calls emptyTable');
 test.todo('in a game placeCardsOnTable calls checkHand');
 test.todo('in a game with hands of equivalent order and value, a reshuffle occurs');
-test.todo('playManyGames will play the specified amount of games');
-test.todo('promiseManyGames will play the specified amount of games');
