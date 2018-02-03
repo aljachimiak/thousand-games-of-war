d353a081bb9e9b3bf90dd9fc21dfdb0cc27e0680
Al Jachimiak <aljachimiak@gmail.com>
2018-02-03 10:58:50 -0500

    adds a test for using the passed in deck

--- test/game-test.js
+++ test/game-test.js
@@ -1,7 +1,7 @@
 // eslint disable import/default
 
 import test from 'ava';
-// import Cards from '../lib/cards';
+import Cards from '../lib/cards';
 // import Player from '../lib/player';
 import Game from '../lib/game';
 
@@ -25,4 +25,24 @@ test('a new game deals the correct number of cards to each player', t => {
 	const game = new Game();
 	t.is(game.players[0].hand.length, game.players[1].hand.length);
 	t.not(game.players[0].hand.length, 0);
+});
+
+test('a new game with a specified deck option uses the correct deck', t => {
+	t.plan(4);
+	let deck = Cards.makeDeck(10);
+	deck = Cards.shuffle(deck);
+	const referenceDeck = deck.slice(0);
+	const options = {deck};
+	const game = new Game(options);
+	t.is(game.players[0].hand.length, game.players[1].hand.length);
+
+	// the deck has 40 cards, so a hand will have 20
+	t.is(game.players[0].hand.length, 20);
+
+	// deal uses Array.push to put cards in the deck
+	// this places the top of the referenceDeck to
+	// go to the back of the player's hand
+	const lastIndex = game.players[0].hand.length - 1;
+	t.is(game.players[0].hand[lastIndex], referenceDeck[0]);
+	t.is(game.players[1].hand[lastIndex], referenceDeck[1]);
 });
